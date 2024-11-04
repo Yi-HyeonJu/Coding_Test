fff68bdc11b8a2dcbcbeacc46fab4e73509aad47
2_현주 <164320612+Yi-HyeonJu@users.noreply.github.com>
2024-11-04 16:13:20 +0900

    [level 0] Title: 가위 바위 보, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -0,0 +1,11 @@
+function solution(rsp) {
+    return rsp
+        .split("")
+        .map((num) => {
+            if(num === '2') return '0';
+            if(num === '0') return '5';
+            if(num === '5') return '2';
+              return num
+             })
+        .join("");
+}
